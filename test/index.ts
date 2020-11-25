f7613d5078ad3a983ff419c440d1c9291d1f12fc
Ganindu <ganindudananja@gmail.com>
2020-11-25 15:18:51 +0530

    Testing started

--- test/index.ts
+++ test/index.ts
@@ -1,1 +1,7 @@
-// import PeraSwarm from 'pera-swarm';
+// GOAL : import PeraSwarm from 'pera-swarm';
+
+const { Coordinate } = require('../lib/coordinate');
+
+const coordinate = new Coordinate(1, 23, 12.0, 19.5);
+
+console.log(coordinate.values);
